4616d4b3df3f944d6aa99c9ae72c349e86ce1fc1
larssiebig <lars.siebig.ls@gmail.com>
2024-04-08 02:05:32 +0200

    initial commit

---
+++ CurrencyConverter/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace CurrencyConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Currency Converter! \n What currency do you want to exchange into USD? \n [1] EUR \n [2] GBP \n [3] Yen");
+            var currencyToConvert = (Currency)int.Parse(Console.ReadLine());
+
+            Console.WriteLine("How much do you want to convert?");
+            var amountToConvert = double.Parse(Console.ReadLine());
+
+            var convertedAmount = $"{DollarExchange.ConvertToUSD(amountToConvert, currencyFrom)}";
+            Console.WriteLine($"Your Value equals {convertedAmount} USD");
+        }
+    }
+
+    static class DollarExchange
+    {
+        public static double ConvertToUSD(double amountValue, Currency currency)
+        {
+            double newAmount;
+            switch (currency)
+            {
+                case Currency.EUR:
+                    return newAmount = amountValue * 1.08;
+                case Currency.GBP:
+                    return newAmount = amountValue * 1.26;
+                case Currency.Yen:
+                    return newAmount = amountValue * 0.0066;
+            }
+        }
+    }
+
+    enum Currency
+    {
+        EUR = 1,
+        GBP = 2,
+        Yen = 3
+    }
+}
